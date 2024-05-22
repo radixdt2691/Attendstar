2279947ffdfb6620c5496e2e0c30ead3688156ff
Jainish Gajjar <jainish.gajjar@radixweb.com>
2024-05-22 12:27:46 +0530

    rearranged:(files)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./assets/**/*.{html,js,woff,woff2}",
-            "./index.html",
+  content: ["../assets/**/*.{html,js,woff,woff2}",
+            "./src/index.html",
             "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
